706106572962c0b3dfb311df1f74ba863d5197b4
zeff96 <zeffo1975@gmail.com>
2023-06-07 11:27:06 +0300

    Add typo fix

--- db/migrate/20230607081327_create_posts.rb
+++ db/migrate/20230607081327_create_posts.rb
@@ -4,8 +4,8 @@ class CreatePosts < ActiveRecord::Migration[7.0]
       t.references :author, null: false, foreign_key: {to_table: :users}, index: true
       t.string :title
       t.text :text
-      t.string :comments_counter
-      t.string :likes_counter
+      t.integer :comments_counter
+      t.integer :likes_counter
 
       t.timestamps
     end
