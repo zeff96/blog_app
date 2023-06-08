cb1368aa250caa9b5de4b8bf8037dbc576cdf930
zeff96 <zeffo1975@gmail.com>
2023-06-08 10:34:51 +0300

    Add method to return last three post

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
     has_one :posts
     has_many :comments, through: :posts
     has_many :likes, through: :posts
+
+    def recent_three_post
+        posts.last(3)
+    end
 end
