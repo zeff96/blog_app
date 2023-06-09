0c5dc996c47867e7713fce450106599f49c65fb3
zeff96 <zeffo1975@gmail.com>
2023-06-09 07:37:16 +0300

    Add post title test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:example) do
+    @first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+  end
+
+  subject {Post.create(author: @first_user, title: 'Hello', text: 'This is my first post')}
+
+  it 'title should be present' do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
 end
