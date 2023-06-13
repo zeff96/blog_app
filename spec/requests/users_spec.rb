3ef635ad571c4d873f159de2b4d76f0e26b17839
zeff96 <zeffo1975@gmail.com>
2023-06-13 14:40:43 +0300

    Add fix for extra blank lines

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -19,7 +19,6 @@ RSpec.describe 'Users', type: :request do
     end
   end
 
-
   describe 'GET /users/:id' do
     let(:user) { User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.') }
 
