581c9c1956410e5a67606d0b67e23b84e3cdbd70
zeff96 <zeffo1975@gmail.com>
2023-06-13 12:35:17 +0300

    Add rails-controller-testing gem

--- Gemfile
+++ Gemfile
@@ -55,6 +55,7 @@ group :development, :test do
   gem 'debug', platforms: %i[mri mingw x64_mingw]
   gem 'dotenv-rails'
   gem 'rspec-rails'
+  gem 'rails-controller-testing'
 end
 
 group :development do
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -5,9 +5,13 @@ RSpec.describe 'Users', type: :request do
     before do
       get users_path
     end
-    
+
     it 'returns http success' do
       expect(response).to have_http_status(:success)
+    end
+
+    it 'renders index template' do
+      expect(response.body).to render_template(:index)
     end
   end
 end
