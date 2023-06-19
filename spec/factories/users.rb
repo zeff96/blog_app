ac3393e5ba7fb2cbd6f14bc26775e701076f3c5a
zeff96 <zeffo1975@gmail.com>
2023-06-19 21:52:13 +0300

    Add post creation counter to test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,15 @@ FactoryBot.define do
     name { 'Tom' }
     photo { 'https://unsplash.com/photos/F_-0BxGuVvo' }
     bio { 'Teacher from Mexico.' }
+
+    factory :user_with_posts do
+      transient do
+        post_count {3}
+      end
+  
+      after(:create) do |user, evaluator|
+        create_list(:post, evaluator.post_count, user: user)
+      end
+    end
   end
 end
---
+++ spec/views/users/show.html.erb_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe 'user_show_page', type: :feature do
+  
+end
