5468b9b497a726c901bdb199eda7b1976ddd83ec
zeff96 <zeffo1975@gmail.com>
2023-06-20 14:11:56 +0300

    Add test validation for comments username

--- spec/views/posts/show.html.erb_spec.rb
+++ spec/views/posts/show.html.erb_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
-post = FactoryBot.create(:post)
+post = FactoryBot.create(:post_with_comments)
+comment = FactoryBot.create(:comment)
 
 RSpec.describe 'posts/show.html.erb', type: :feature do
   scenario 'display post title' do
@@ -26,5 +27,10 @@ RSpec.describe 'posts/show.html.erb', type: :feature do
   scenario 'display post text' do
     visit user_post_path(post.author, post)
     expect(page).to have_content(post.text)
+  end
+
+  scenario 'display username of each commenter' do
+    visit user_posts_path(post.author, post)
+    expect(page).to have_content(comment.author.name)
   end
 end
