58b768739e3930b88381cb5658cd47c145b5ef2a
zeff96 <zeffo1975@gmail.com>
2023-06-20 18:04:45 +0300

    Add typo fix

--- spec/views/posts/show.html.erb_spec.rb
+++ spec/views/posts/show.html.erb_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 post = FactoryBot.create(:post_with_comments)
-comment = FactoryBot.create(:comment)
+FactoryBot.create(:comment)
 
 RSpec.describe 'posts/show.html.erb', type: :feature do
   scenario 'display post title' do
@@ -31,7 +31,9 @@ RSpec.describe 'posts/show.html.erb', type: :feature do
 
   scenario 'display username of each commenter' do
     visit user_posts_path(post.author, post)
-    expect(page).to have_content(comment.author.name)
+    post.comments.each do |post_comment|
+      expect(page).to have_content(post_comment.author.name)
+    end
   end
 
   scenario 'display username of each commenter' do
