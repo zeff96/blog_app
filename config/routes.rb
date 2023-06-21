d44dfb6f1c75f8858cb8d80c2b855c33b575042e
zeff96 <zeffo1975@gmail.com>
2023-06-21 11:10:16 +0300

    Add routes typo fix

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     authenticated :user do
-      root to: 'user#index', as: :authenticated_root
+      root to: 'users#index', as: :authenticated_root
     end
   
     unauthenticated :user do
