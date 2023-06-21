45a4c940fd8420e3b682eb4d431c6d320051e473
zeff96 <zeffo1975@gmail.com>
2023-06-21 17:17:03 +0300

    Add post authorization

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  load_and_authorize_resource
+  
   def index
     @user = User.find(params[:user_id])
     @posts = Post.includes(:author, :comments).where(author_id: params[:user_id])
