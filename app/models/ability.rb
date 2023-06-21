7c199c9af8eec88bcb72446390f9161eb95e0bb7
zeff96 <zeffo1975@gmail.com>
2023-06-22 00:32:05 +0300

    Add user comment authorization

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,7 +5,9 @@ class Ability
     return unless user.present?
 
     can :read, Post
+    can :read, Comment
     can :manage, Post, author_id: user.id
+    can :manage, Comment, author_id: user.id
 
     return unless user.admin?
 
