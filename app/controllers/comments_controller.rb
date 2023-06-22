0158713f56ace72de3af93169ff70fb1b5e27f8c
shasherazi <hassanrandomz@gmail.com>
2023-06-22 20:17:02 +0500

    Remove CSRF validation for creating comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   load_and_authorize_resource
+  skip_before_action :verify_authenticity_token, only: [:create]
 
   def index
     @user = User.find(params[:user_id])
