41bd18cefbb75c937bc834bc6b0c7d4fb36aa4b5
shasherazi <hassanrandomz@gmail.com>
2023-06-23 09:41:38 +0500

    Remove unnecessary html renders

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -12,10 +12,8 @@ class Api::CommentsController < ApplicationController
     @comment = post.comments.new(author: current_user, **comment_params)
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to user_post_path(post.author, post), notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
       else
-        format.html { redirect_to user_post_path(post.author, post), notice: 'Comment was not created.' }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
